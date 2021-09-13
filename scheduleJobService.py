569bb8e8c8373b21db8fa97d32ab7d943bbb92a7
Donia HARIZ <68640183+dHARIZ-25@users.noreply.github.com>
2021-09-13 03:41:53 -0900

    Add files via upload

---
+++ scheduleJobService.py
@@ -0,0 +1,294 @@
+#https://smart-irrigation.azurewebsites.net/api/smartirrigation?name=start
+import sys
+import time
+import threading
+import uuid
+
+from azure.iot.hub import IoTHubRegistryManager
+from azure.iot.hub.models import Twin, TwinProperties, CloudToDeviceMethod, CloudToDeviceMethodResult, QuerySpecification, QueryResult
+
+CONNECTION_STRING = "HostName=smart-irrigation.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=LLzg1wPIm67RAXYozZNlfAY911OFSniw77422UNzgN8="
+DEVICE_ID = "raspberrypi"
+
+method_name = input("If you want to start watering type START if you want to stop it type STOP \nYou can also type AUTO if you want to switch to the automatic watering mode \n \n")
+print(method_name)
+
+#choose manually either to start or stop watering
+#start watering
+if method_name == "START":
+    METHOD_NAME = "StartWatering"
+    METHOD_PAYLOAD = "{\"pumpTime\":\"1s\"}"
+    UPDATE_PATCH = {"Plant":"flower"}
+    TIMEOUT = 60
+    WAIT_COUNT = 1
+    def query_condition(iothub_registry_manager, device_id):
+
+        query_spec = QuerySpecification(query="SELECT * FROM devices WHERE deviceId = '{}'".format(device_id))
+        query_result = iothub_registry_manager.query_iot_hub(query_spec, None, 1)
+
+        return len(query_result.items)
+
+    def device_method_job(job_id, device_id, wait_time, execution_time):
+        print ( "" )
+        print ( "Scheduling job: " + str(job_id) )
+        time.sleep(wait_time)
+
+        iothub_registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+
+        if query_condition(iothub_registry_manager, device_id):
+            deviceMethod = CloudToDeviceMethod(method_name=METHOD_NAME, payload=METHOD_PAYLOAD)
+
+            response = iothub_registry_manager.invoke_device_method(DEVICE_ID, deviceMethod)
+
+            print ( "" )
+            print ( "Direct method " + METHOD_NAME + " called." )
+
+    def device_twin_job(job_id, device_id, wait_time, execution_time):
+        print ( "" )
+        print ( "Scheduling job " + str(job_id) )
+        time.sleep(wait_time)
+
+        iothub_registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+        if query_condition(iothub_registry_manager, device_id):
+
+            twin = iothub_registry_manager.get_twin(DEVICE_ID)
+            twin_patch = Twin(properties= TwinProperties(desired=UPDATE_PATCH))
+            twin = iothub_registry_manager.update_twin(DEVICE_ID, twin_patch, twin.etag)
+
+            print ( "" )
+            print ( "Device twin updated." )
+
+    def iothub_jobs_sample_run():
+        try:
+            method_thr_id = uuid.uuid4()
+            method_thr = threading.Thread(target=device_method_job, args=(method_thr_id, DEVICE_ID, 20, TIMEOUT), kwargs={})
+            method_thr.start()
+
+            print ( "" )
+            print ( "Direct method called with Job Id: " + str(method_thr_id) )
+
+            twin_thr_id = uuid.uuid4()
+            twin_thr = threading.Thread(target=device_twin_job, args=(twin_thr_id, DEVICE_ID, 10, TIMEOUT), kwargs={})
+            twin_thr.start()
+
+            print ( "" )
+            print ( "Device twin called with Job Id: " + str(twin_thr_id) )
+
+            while True:
+                print ( "" )
+
+                if method_thr.is_alive():
+                    print ( "...job " + str(method_thr_id) + " still running." )
+                else:
+                    print ( "...job " + str(method_thr_id) + " complete." )
+
+                if twin_thr.is_alive():
+                    print ( "...job " + str(twin_thr_id) + " still running." )
+                else:
+                    print ( "...job " + str(twin_thr_id) + " complete." )
+
+                print ( "Job status posted, press Ctrl-C to exit" )
+
+                status_counter = 0
+                while status_counter <= WAIT_COUNT:
+                    time.sleep(1)
+                    status_counter += 1
+
+        except Exception as ex:
+            print ( "" )
+            print ( "Unexpected error {0}" % ex )
+            return
+        except KeyboardInterrupt:
+            print ( "" )
+            print ( "IoTHubService sample stopped" )
+
+#stop watering    
+elif method_name == "STOP":
+    METHOD_NAME = "StopWatering"
+    METHOD_PAYLOAD = "{\"pumpTime\":\"1s\"}"
+    UPDATE_PATCH = {"Plant":"flower"}
+    TIMEOUT = 60
+    WAIT_COUNT = 5
+    def query_condition(iothub_registry_manager, device_id):
+
+        query_spec = QuerySpecification(query="SELECT * FROM devices WHERE deviceId = '{}'".format(device_id))
+        query_result = iothub_registry_manager.query_iot_hub(query_spec, None, 1)
+
+        return len(query_result.items)
+
+    def device_method_job(job_id, device_id, wait_time, execution_time):
+        print ( "" )
+        print ( "Scheduling job: " + str(job_id) )
+        time.sleep(wait_time)
+
+        iothub_registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+
+        if query_condition(iothub_registry_manager, device_id):
+            deviceMethod = CloudToDeviceMethod(method_name=METHOD_NAME, payload=METHOD_PAYLOAD)
+
+            response = iothub_registry_manager.invoke_device_method(DEVICE_ID, deviceMethod)
+
+            print ( "" )
+            print ( "Direct method " + METHOD_NAME + " called." )
+
+    def device_twin_job(job_id, device_id, wait_time, execution_time):
+        print ( "" )
+        print ( "Scheduling job " + str(job_id) )
+        time.sleep(wait_time)
+
+        iothub_registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+        if query_condition(iothub_registry_manager, device_id):
+
+            twin = iothub_registry_manager.get_twin(DEVICE_ID)
+            twin_patch = Twin(properties= TwinProperties(desired=UPDATE_PATCH))
+            twin = iothub_registry_manager.update_twin(DEVICE_ID, twin_patch, twin.etag)
+
+            print ( "" )
+            print ( "Device twin updated." )
+
+    def iothub_jobs_sample_run():
+        try:
+            method_thr_id = uuid.uuid4()
+            method_thr = threading.Thread(target=device_method_job, args=(method_thr_id, DEVICE_ID, 20, TIMEOUT), kwargs={})
+            method_thr.start()
+
+            print ( "" )
+            print ( "Direct method called with Job Id: " + str(method_thr_id) )
+
+            twin_thr_id = uuid.uuid4()
+            twin_thr = threading.Thread(target=device_twin_job, args=(twin_thr_id, DEVICE_ID, 10, TIMEOUT), kwargs={})
+            twin_thr.start()
+
+            print ( "" )
+            print ( "Device twin called with Job Id: " + str(twin_thr_id) )
+
+            while True:
+                print ( "" )
+
+                if method_thr.is_alive():
+                    print ( "...job " + str(method_thr_id) + " still running." )
+                else:
+                    print ( "...job " + str(method_thr_id) + " complete." )
+
+                if twin_thr.is_alive():
+                    print ( "...job " + str(twin_thr_id) + " still running." )
+                else:
+                    print ( "...job " + str(twin_thr_id) + " complete." )
+
+                print ( "Job status posted, press Ctrl-C to exit" )
+
+                status_counter = 0
+                while status_counter <= WAIT_COUNT:
+                    time.sleep(1)
+                    status_counter += 1
+
+        except Exception as ex:
+            print ( "" )
+            print ( "Unexpected error {0}" % ex )
+            return
+        except KeyboardInterrupt:
+            print ( "" )
+            print ( "IoTHubService sample stopped" )
+
+#automatic watering
+elif method_name == "AUTO":
+    METHOD_NAME = "AutomaticWatering"
+    METHOD_PAYLOAD = "{\"pumpTime\":\"1s\"}"
+    UPDATE_PATCH = {"Plant":"flower"}
+    TIMEOUT = 60
+    WAIT_COUNT = 5
+    
+
+    def query_condition(iothub_registry_manager, device_id):
+
+        query_spec = QuerySpecification(query="SELECT * FROM devices WHERE deviceId = '{}'".format(device_id))
+        query_result = iothub_registry_manager.query_iot_hub(query_spec, None, 1)
+
+        return len(query_result.items)
+
+    def device_method_job(job_id, device_id, wait_time, execution_time):
+        print ( "" )
+        print ( "Scheduling job: " + str(job_id) )
+        time.sleep(wait_time)
+
+        iothub_registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+
+        if query_condition(iothub_registry_manager, device_id):
+            deviceMethod = CloudToDeviceMethod(method_name=METHOD_NAME, payload=METHOD_PAYLOAD)
+
+            response = iothub_registry_manager.invoke_device_method(DEVICE_ID, deviceMethod)
+
+            print ( "" )
+            print ( "Direct method " + METHOD_NAME + " called." )
+
+    def device_twin_job(job_id, device_id, wait_time, execution_time):
+        print ( "" )
+        print ( "Scheduling job " + str(job_id) )
+        time.sleep(wait_time)
+
+        iothub_registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+        if query_condition(iothub_registry_manager, device_id):
+
+            twin = iothub_registry_manager.get_twin(DEVICE_ID)
+            twin_patch = Twin(properties= TwinProperties(desired=UPDATE_PATCH))
+            twin = iothub_registry_manager.update_twin(DEVICE_ID, twin_patch, twin.etag)
+
+            print ( "" )
+            print ( "Device twin updated." )
+
+    def iothub_jobs_sample_run():
+        try:
+            method_thr_id = uuid.uuid4()
+            method_thr = threading.Thread(target=device_method_job, args=(method_thr_id, DEVICE_ID, 20, TIMEOUT), kwargs={})
+            method_thr.start()
+
+            print ( "" )
+            print ( "Direct method called with Job Id: " + str(method_thr_id) )
+
+            twin_thr_id = uuid.uuid4()
+            twin_thr = threading.Thread(target=device_twin_job, args=(twin_thr_id, DEVICE_ID, 10, TIMEOUT), kwargs={})
+            twin_thr.start()
+
+            print ( "" )
+            print ( "Device twin called with Job Id: " + str(twin_thr_id) )
+
+            while True:
+                print ( "" )
+
+                if method_thr.is_alive():
+                    print ( "...job " + str(method_thr_id) + " still running." )
+                else:
+                    print ( "...job " + str(method_thr_id) + " complete." )
+
+                if twin_thr.is_alive():
+                    print ( "...job " + str(twin_thr_id) + " still running." )
+                else:
+                    print ( "...job " + str(twin_thr_id) + " complete." )
+
+                print ( "Job status posted, press Ctrl-C to exit" )
+
+                status_counter = 0
+                while status_counter <= WAIT_COUNT:
+                    time.sleep(1)
+                    status_counter += 1
+
+        except Exception as ex:
+            print ( "" )
+            print ( "Unexpected error {0}" % ex )
+            return
+        except KeyboardInterrupt:
+            print ( "" )
+            print ( "IoTHubService sample stopped" )
+
+if __name__ == '__main__':
+    print ( "Starting the IoT Hub jobs Python sample..." )
+    print ( "    Connection string = {0}".format(CONNECTION_STRING) )
+    print ( "    Device ID         = {0}".format(DEVICE_ID) )
+
+    iothub_jobs_sample_run()
